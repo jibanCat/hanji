7255d3485685b7e8f5b226fa69beb6c1fdc17f1d
jibanCat <r04244007@ntu.edu.tw>
2018-07-10 00:23:09 +0800

    initial commit, copy DigitalHumanities/HanJi to this repo

---
+++ README.md
@@ -0,0 +1,116 @@
+# Han-Ji (漢籍) Scraper
+
+- The scraper function is defined as a method in the `Book` class. You can scrape the html files using
+
+```python
+from Book
+# get a instance of Book class
+book = Book(bookname="name", date="2018-05-29", creator="MF)
+
+# get the htmls from the Han-Ji website, The UR
+book.fetch_data('http://hanchi.ihp.sinica.edu.tw/ihpc/hanji?@30^1389784921^802^^^60311004001000010006@@460127924',
+                pages_limit=1000, print_bookmark=False,)
+```
+
+- Now, `bs4` objects are stored in the `book.flat_bodies` list. You can write the html files into `data` folder via
+
+```python
+# writing htmls into a folder
+book.write_htmls(path="data")
+
+# loading files to book
+book.load_htmls(path="data")
+```
+
+- Exctract the bookmark (the dependencies of the poems) out of the page, just typed
+
+```python 
+book.extract_paths()
+```
+
+- To preview the first page of Han-Ji page in a pretty HTML format, type
+
+```python
+book.pretty_print(0) # 0 for the first page in scraped Han-Ji data
+```
+
+## WenShuan (文選) Organizer
+
+- The `WenShuan.py` was designed as a wrapper of the `Book.py` and have specific methods to organize the texts files in WenShuan
+
+```python 
+# get a instance out of WenShuan class
+wenshuan = WenShuan('2018-05-29', 'MF')
+wenshuan.fetch_data(URL="(URL for Han-Ji WenShuan)",
+                pages_limit=1000, print_bookmark=True,)
+
+# organize the text files 
+wenshuan.extract_paths()         # extract the bookmarks
+wenshuan.get_author_bag()        # get the bag of author names and comments
+wenshuan.extract_meta()          # extract the meta data
+wenshuan.passages2tuples()       # get the passsage into (text, comment) tuples
+wenshuan.heads2tuples()          # get headers into (head, comment, ...) tuples
+wenshuan.extract_commentators()  # append commentators to metadata
+wenshuan.extract_sound_glosses() # append all sound glosses in comments into a list and remove them from the self.flat_passages
+```
+- Writing to CSV: `Wenshuan.py` provides a method to write `wenshuan.flat_passages` and `wenshuan.flat_meta` to a series of CSV files in folder (default folder is `"/文選"`). Metadata is listed in the comments (`#`) in the headers. 
+
+```python
+wenshuan.write_passages_ECSV()
+```
+
+
+## (Ongoing) SongShu (宋書) Organizer 
+
+- The `SongShu.py` was also designed as a wrapper of `Book.py` class. SongShu organizer separated every pieces of works into passages. 
+
+```python
+from SongShu import SongShu
+songshu = SongShu("2018-06-28", "MF")
+songshu.fetch_data(URL="(The first page URL of SongShu in Han-Ji)", pages_limit=2000, print_bookmark=True)
+songshu.write_htmls()
+```
+
+- To recover the fetched data we downloaded last time, run
+```python
+songshu = SongShu("2018-06-28", "MF")
+songshu.load_htmls()
+# [Info] Stop at loading data/ShongShu_0851.html.
+# [Info] Total length of the data is 851.
+```
+
+- To extract metadata, bookmarks, and organize the passages:
+```python
+# preprocessing the songshu data to get metadata and bookmarks
+# and separate the passages in every pages
+songshu.extract_paths()
+songshu.extract_meta()
+songshu.extract_passages()
+```  
+The <font color="#A60628">Warning</font> in the above output cell show that some pages in SonShu do not have a clear definition of **separating the passages**.
+
+
+## Rare Character Identifier
+
+Some characters in Han-Ji are rare chars. In this case, if we use `urllib` to parse the source page, we only get the fragments of the rare chars (構字形). 
+
+To resolve this situation, we can use JavaScript API in http://char.iis.sinica.edu.tw/ to acquire the fragments of chars, and then we can use the fragments to search the correct rare char unicodes. 
+
+The following lines show how to scrape the a bag of rare char unicodes from a text string:
+```python
+from rare_char_converter import rare_char_converter
+
+selenium_driver = "(PATH TO YOUR SELENIUM DRIVER)"
+text = "(YOUR HAN-JI TEXT)"
+rare_char_converter(text, selenium_driver)
+# Return: dict, {"(fragments of char)" : (UNICODE, string of the rare char)}
+```
+
+## Tutorial List:
+
+1. [`Book -- Basic Usage.ipynb`](http://nbviewer.jupyter.org/github/jibanCat/DigitalHumanities/blob/master/Han-Ji/tutorials/Book%20--%20Basic%20Usage.ipynb): The basic usage of `Book.py`. The way to scrape a book in Han-Ji and class methods to do further exploration.
+2. [`WenShuan -- How to Use and Sound Glosses Search.ipynb`](http://nbviewer.jupyter.org/github/jibanCat/DigitalHumanities/blob/master/Han-Ji/tutorials/WenShuan%20--%20How%20to%20Use%20and%20Sound%20Glosses%20Search.ipynb): How to use WenShuan class and an exploration for sound glosses in WenShuan.
+3. [`WenShuan -- What happens to the number of separate pieces (76X comparing to 528).ipynb`](http://nbviewer.jupyter.org/github/jibanCat/DigitalHumanities/blob/master/Han-Ji/tutorials/WenShuan%20--%20What%20happens%20to%20the%20number%20of%20separate%20pieces%20%2876X%20comparing%20to%20528%29.ipynb): A tutorial for counting the number of piece works in WenShuan.
+4. [`WenShuan -- Rare Char Identify Using Selenium.ipynb`](http://nbviewer.jupyter.org/github/jibanCat/DigitalHumanities/blob/master/Han-Ji/tutorials/WenXuan%20--%20Rare%20Char%20Identify%20Using%20Selenium.ipynb): Identify the rare characters in WenShuan using rare chars search in sinica.
+5. [`SongShu -- Place Names Export (Part 1).ipynb`](http://nbviewer.jupyter.org/github/jibanCat/DigitalHumanities/blob/master/Han-Ji/tutorials/SongShu%20--%20Place%20Names%20Export%20%28Part%201%29.ipynb): How to use Songshu class and simplest regex to match all place names in the SongShu class.
+6. [`SongShu -- Place Names Export (Part 2).ipynb`](http://nbviewer.jupyter.org/github/jibanCat/DigitalHumanities/blob/master/Han-Ji/tutorials/SongShu%20--%20Place%20Names%20Export%20%28Part%202%29.ipynb): Export place names to `csv`s and data viz the frequency of characters associated with place names.
---
+++ hanji/Book.py
@@ -0,0 +1,225 @@
+from collections import defaultdict
+### WHY "defaultdict", INSTEAD OF THE REGULAR "dict" FUNCTION?
+from datetime import datetime
+from bs4 import BeautifulSoup
+import bs4
+from urllib import request
+import urllib
+import time
+import random
+import re
+import os
+import glob
+
+
+class Book:
+    """Han-Ji '<http://hanchi.ihp.sinica.edu.tw/ihp/hanji.htm>'_ Dataset.
+    
+    Attributes:
+        flat_bodies (list): a list containing all htmls 
+        flat_passages (list): a list containing the text of all passages (i.e., every individual piece in a book). Users should define their own methods to organize the passages.
+        flat_heads (list): a list containing all the text of the heads (i.e., the metadata at the top of each individual piece, like title and author). Users should define their own methods to organize the heads.
+        flat_meta (list): a list containing all metadata (dictionary) extracted from bookmarks. User should define their own methods to extract metadata.
+        paths (list): a list of paths extracted from the "bookmark" provided in the database. e.g., 集／總集／文選／卷第二十七　詩戊之一／樂府上／古樂府三首／飲馬長城窟行(P.1277)
+    
+    Args: 
+        bookname (string): the name of the book, default = ''
+        date (string): the date you collected the book, default = None
+        creator (string): the name of the creator who created the instance
+        description (string): optional description for the instance
+        
+    Methods:
+        fetch_data(URL): fetch book bs4 obj from a start page URL of a Book in Han-Ji
+        extract_paths(): extract paths from bookmark in self.flat_bodies list and append paths to self.paths
+        write_htmls(path): write data into htmls on the disk in path
+        load_htmls(path): load data from htmls on the disk in path
+    """
+    
+    def __init__(self, bookname='', date=None, creator=None, description=''):
+        self.flat_bodies   = []
+        self.flat_passages = []
+        self.flat_heads    = []
+        self.flat_meta     = []
+        self.paths = []
+        self.author_bag = defaultdict(list)
+        self.bookname    = bookname
+        self.date        = datetime.strptime(date, '%Y-%m-%d')
+        self.creator     = creator
+        self.description = description
+        ### ?
+        
+    def __getitem__(self, index):
+        '''
+        Args:
+            index (int): Index
+            
+        Returns:
+            bs4 html object in the flat_bodies
+        '''
+        return self.flat_bodies[index]
+    
+    def __len__(self):
+        return len(self.flat_bodies)
+    
+    def __repr__(self):
+        fmt_str = "Dataset {} ".format(self.bookname)
+        fmt_str += "created by {} at {}.\n".format(self.creator, self.date)
+        fmt_str += "{} data points. ".format(self.__len__()) 
+        if len(self.author_bag) > 2:
+            fmt_str += "\n{} authors and commentars.\n".format(len(self.author_bag))
+        fmt_str += self.description
+        return fmt_str
+
+    def pretty_print(self, index):
+        """pretty print the html source page in a Jupyter notebook cell output"""
+        from IPython.display import HTML
+        return HTML(self._pretty_html( self.flat_bodies[index] ))
+
+    def _pretty_html(self, soup):
+        """cut off irrelevant content, such as side columns in the webpage, from the Han-Ji HTML source page. 
+        This procedure aims to save memory for the computer."""
+        head = soup.find("head")
+        span_id_fontstyle = str(soup.find("span", {"id": "fontstyle"}))
+        path  = str(soup.find('a', attrs={'class', 'gobookmark'}))
+        HTML_string = """<html>
+                {}
+            <body>
+                {}
+            </body>
+        </html>
+        """.format(str(head), "{}\n\t{}".format(path, span_id_fontstyle))
+        return HTML_string
+    
+    def fetch_data(self, URL, pages_limit=10000, print_bookmark=False, html_cutoff=False,
+                   BASE_URL='http://hanchi.ihp.sinica.edu.tw/ihpc/', sleep_range=(1, 3)):
+        '''fetch book bs4 obj from a start page URL of a Book in Han-Ji
+        
+        Args:
+            URL (string): the start page url from han-ji website
+            page_limit (int): the limit of next pages you can scrape. default = 10000
+            print_bookmark (bool): print the bookmark while fetching the data. default = False
+            html_cutoff (bool): cut off the irrelavant side column and tags in Han-Ji raw html files, 
+                                to save memory usage.
+        '''
+        for i in range(pages_limit):            
+            # use urllib.request to get the html content of the website
+            req  = request.Request(URL, headers={'User-Agent': 'Mozilla/5.0'})
+            page = request.urlopen(req)
+            soup = BeautifulSoup(page, 'lxml')
+
+            # show information on the screen
+            if print_bookmark == True:
+                print("[Info] Start fetching {}. {}/{} epoch.".format(
+                    soup.find('a', attrs={'class', 'gobookmark'}).text, i + 1, pages_limit))            
+            else:
+                print("[Info] Start fetching {}. {}/{} epoch.".format(URL, i + 1, pages_limit))            
+            
+            # check if the content is the same as previous page
+            ### ? -> Response: this line is an ad-hoc solution for dealing with the first page while scraping. There must be a better way to do it.
+            if i > 0:
+                buffer = self.flat_bodies[-1].find_all('div', attrs={'style': True})
+            else:
+                # use a dummy list for the buffer for the first page
+                buffer = ['dummy']
+            
+            # if the first and last elements in the buffer are the same as current page
+            # delete page and save the current page.
+            ### GOOD SOLUTION, BUT ARE WE SURE THERE ARE NO HIDDEN TRAPS IN USING THIS RULE?  COULD TWO CONSECUTIVE BUT DIFFERENT POEMS HAVE THE SAME START AND END WORD?
+            ### Response: the comparison here is for end and start sentences of a poem. 
+            ### It's quite unlikely two poems have the same start and end senetences, right?
+            if (buffer[-1] == 
+                soup.find_all('div', attrs={'style': True})[-1]) and (
+                buffer[0] == 
+                soup.find_all('div', attrs={'style': True})[0]):
+                print("[Warning] This page is the same as the previous one, discard previous one and store the new one.")
+                if html_cutoff == True:
+                    self.flat_bodies[-1] = BeautifulSoup( self._pretty_html(soup), 'lxml' )
+                else:    
+                    self.flat_bodies[-1] = soup
+            else:
+                # append to flat bodies
+                if html_cutoff==True:
+                    self.flat_bodies.append( BeautifulSoup( self._pretty_html(soup), 'lxml'))
+                else:
+                    self.flat_bodies.append(soup)
+               
+            
+            # find the next page
+            next_page = soup.find('img', {'src' : '/ihp/snext.gif'})
+            if next_page != None:
+                url = next_page.find_parent()['href']
+            else:
+                print('[Info] No further next page. Stop fetching.')
+                break
+                
+            URL = urllib.parse.urljoin(BASE_URL, url)
+            time.sleep(random.randint(sleep_range[0], sleep_range[1]))
+            
+    def extract_paths(self):
+        '''extract paths from bookmark in self.flat_bodies list and append paths to self.paths'''
+        self.paths = []
+        
+        for soup in self.flat_bodies:
+            # extract "gobookmark" class
+            path  = soup.find('a', attrs={'class', 'gobookmark'}).text
+            self.paths.append(path)
+    
+    def _sum_indent_and_padding(self, texts):
+        '''returns the sum of indents and paddings in the texts.'''
+        return [
+            sum([int(num[0]), int(num[1])])
+             for text in texts 
+             for num in re.findall(r'text-indent:(.*?)em;padding-left:(.*?)em;', text['style'])
+        ]        
+
+    def _indent_and_padding(self, texts):
+        '''Return the indent and padding tuples of indents and paddings in the texts.'''
+        return [
+            (int(num[0]), int(num[1]))
+             for text in texts 
+             for num in re.findall(r'text-indent:(.*?)em;padding-left:(.*?)em;', text['style'])
+        ]                    
+                                        
+    def write_htmls(self, path='data/', html_cutoff=False):
+        '''writing all htmls in flat_bodies to the folder data/
+
+        Args:
+            path (str) : the path to the folder you want to write htmls files
+            html_cutoff (bool) : whether or not you want to cut off irrelevant contents in Han-Ji webpage 
+        '''
+        try:
+            os.makedirs(path)
+        except OSError:
+            pass
+            
+        for i,soup in enumerate(self.flat_bodies):
+            filename = os.path.join(path, '{}_{}.html'.format(
+                self.bookname, str(i).zfill(4)))
+            with open(filename, 'w', encoding='utf-8') as file:
+                if html_cutoff==True:
+                    file.write( self._pretty_html(soup) )
+                else:
+                    file.write(str(soup))
+                
+        # update the description
+        self.description += 'Writing data to {}.\n'.format(os.path.join(path, self.bookname + '*'))
+        
+    def load_htmls(self, path='data/'):
+        '''loading all files with filename = "bookname_*.html" in path data/
+        '''
+        self.flat_bodies = []
+        i = 0
+        while 1:
+            filename = os.path.join(path, '{}_{}.html'.format(
+                self.bookname, str(i).zfill(4)))
+            if os.path.isfile(filename):
+                with open(filename, 'r', encoding='utf-8') as file:
+                    self.flat_bodies.append(BeautifulSoup(file.read(), 'lxml'))
+            else:
+                print("[Info] Stop at loading {}.".format(filename))
+                break
+            i += 1
+        print("[Info] Total length of the data is {}.".format(len(self.flat_bodies)))
+        
+        # update the description
+        self.description += 'Loading data from {}.\n'.format(path.format(os.path.join(path, self.bookname + '*')))
